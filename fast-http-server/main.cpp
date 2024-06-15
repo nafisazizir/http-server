b086270688b8dae54bfdb87328220cb7739de316
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-06-15 13:32:44 +1000

    Initial Commit

---
+++ fast-http-server/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fast-http-server
+//
+//  Created by Nafis Azizi Riza on 15/06/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
