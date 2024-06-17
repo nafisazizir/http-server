e39bf64a4d0e61c538de78889d29dcf97cb2435b
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-06-17 16:56:01 +1000

    feat: add cmake and refactor

---
+++ build/CMakeFiles/HttpServer.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nafisaziziriza/Project/fast-http-server/src/http_enums.cpp" "CMakeFiles/HttpServer.dir/src/http_enums.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/http_enums.cpp.o.d"
+  "/Users/nafisaziziriza/Project/fast-http-server/src/http_message_interface.cpp" "CMakeFiles/HttpServer.dir/src/http_message_interface.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/http_message_interface.cpp.o.d"
+  "/Users/nafisaziziriza/Project/fast-http-server/src/http_request.cpp" "CMakeFiles/HttpServer.dir/src/http_request.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/http_request.cpp.o.d"
+  "/Users/nafisaziziriza/Project/fast-http-server/src/http_response.cpp" "CMakeFiles/HttpServer.dir/src/http_response.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/http_response.cpp.o.d"
+  "/Users/nafisaziziriza/Project/fast-http-server/src/http_server.cpp" "CMakeFiles/HttpServer.dir/src/http_server.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/http_server.cpp.o.d"
+  "/Users/nafisaziziriza/Project/fast-http-server/src/main.cpp" "CMakeFiles/HttpServer.dir/src/main.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/main.cpp.o.d"
+  "/Users/nafisaziziriza/Project/fast-http-server/src/uri.cpp" "CMakeFiles/HttpServer.dir/src/uri.cpp.o" "gcc" "CMakeFiles/HttpServer.dir/src/uri.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/HttpServer.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HttpServer.dir/src/http_enums.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/http_enums.cpp.o.d"
+  "CMakeFiles/HttpServer.dir/src/http_message_interface.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/http_message_interface.cpp.o.d"
+  "CMakeFiles/HttpServer.dir/src/http_request.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/http_request.cpp.o.d"
+  "CMakeFiles/HttpServer.dir/src/http_response.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/http_response.cpp.o.d"
+  "CMakeFiles/HttpServer.dir/src/http_server.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/http_server.cpp.o.d"
+  "CMakeFiles/HttpServer.dir/src/main.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/main.cpp.o.d"
+  "CMakeFiles/HttpServer.dir/src/uri.cpp.o"
+  "CMakeFiles/HttpServer.dir/src/uri.cpp.o.d"
+  "HttpServer"
+  "HttpServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HttpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
